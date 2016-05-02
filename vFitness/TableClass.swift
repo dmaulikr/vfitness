e10b5245a5b31dda17c92f9054492b3e1129f826
Greg Vinall <gregvinall@gregs-macbook.local>
2016-05-02 22:26:00 +1000

    Initial setup of git repository

---
+++ vFitness/TableClass.swift
@@ -0,0 +1,13 @@
+//
+//  TableClass.swift
+//  vFitness
+//
+//  Created by Greg Vinall on 2/05/2016.
+//  Copyright © 2016 V Software. All rights reserved.
+//
+
+import Foundation
+
+struct Table {
+    var tableName = "Dummy"
+}
