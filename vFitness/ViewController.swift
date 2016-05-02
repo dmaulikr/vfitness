24255fbba33817afd61d4f082175836b2d49867d
Greg Vinall <gregvinall@gregs-macbook.fritz.box>
2016-05-02 22:19:20 +1000

    Initial Commit

---
+++ vFitness/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  vFitness
+//
+//  Created by Greg Vinall on 2/05/2016.
+//  Copyright © 2016 V Software. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: AnyObject? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
