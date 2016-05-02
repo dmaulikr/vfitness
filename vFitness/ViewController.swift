6d35044f55ba593159c2ff9cc5c10ad6f609c3e4
Greg Vinall <gregvinall@gregs-macbook.local>
2016-05-03 09:36:41 +1000

    Initial Commit

---
+++ vFitness/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  vFitness
+//
+//  Created by Greg Vinall on 3/05/2016.
+//  Copyright © 2016 V Software. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
